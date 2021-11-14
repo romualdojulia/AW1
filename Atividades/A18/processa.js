45a143191dd125fbb38c8443187f8e4ab511dd6c
Julia Romualdo <75337060+romualdojulia@users.noreply.github.com>
2021-11-14 00:23:00 -0300

    Add files via upload

---
+++ Atividades/A18/processa.js
@@ -0,0 +1,55 @@
+// callback > promise > async/await
+
+const fs = require('fs')
+const { resolve } = require('path/posix')
+const { stringify } = require('querystring')
+
+console.log(1)
+/*
+//callback 
+fs.readFile('./in1.txt', (err, contents) => {
+  fs.readFile('./in2.txt', (err2, contents2) => {
+    console.log(err, String(contents))
+    console.log(err2, String(contents2))
+  })
+})
+*/
+//promise
+const readFile = file =>
+  new Promise((resolve, reject) => {
+    fs.readFile(file, (err, contents) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(contents)
+      }
+    })
+  })
+/*
+//promise
+readFile('./in1.txt')
+  .then(contents => {
+    console.log(String(contents))
+    return readFile('./in2.txt')
+  })
+  .then(contents => {
+    console.log(String(contents))
+  })
+*/
+
+//async/await - açucar sintático em cima da promise
+
+const init = async () => {
+  try {
+    const contents = await readFile('./in1.txt')
+    const contents2 = await readFile('./in2.txt')
+    return String(contents) + '\n' + String(contents2)
+  } catch (err) {
+    console.log(err)
+  }
+}
+init().then(contents => console.log(contents))
+
+console.log(2)
+
+console.log(3)
